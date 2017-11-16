e06780be85c75e9ea3ff42cade021adde423a995
Barry <cherryjiangbiao@qq.com>
2017-11-16 14:07:15 +0800

    1

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -1,13 +1,14 @@
 package com.mmall.common;
 
+
 import java.util.Set;
 
 import com.google.common.collect.Sets;
 
 public class Const {
 	public  static final String CURRENT_USER="currentUser";
-	public  static final String EMAIL="email";
-	public  static final String USERNAME="username";
+	public  static final String EMAIL="email";//关键字类型
+	public  static final String USERNAME="username";//关键字类型
 	public interface Role{//内部类
 		int ROLE_CUSTOMER=0;
 		int ROLE_ADMIN=1;
@@ -22,11 +23,19 @@ public class Const {
 	public interface ProductListOrderBy{//内部类
 		Set<String> PRICE_ASC_DESC=Sets.newHashSet("price_desc","price_asc");
 	}
+	/**
+	 * 
+	 * @ClassName: ProductStatus
+	 * @Description: TODO 销售状态 【内部类】
+	 * @author Barry
+	 * @date 2017年11月16日 下午1:47:30
+	 *
+	 */
 	public enum ProductStatus{		
 		ON_SALE(1,"在售状态"),
 		NO_SALE(0,"下架状态");
-		private int code;
-		private String value;
+		private final int code;
+		private final String value;
 		private ProductStatus( int code,String value) {
 			this.value = value;
 			this.code = code;
